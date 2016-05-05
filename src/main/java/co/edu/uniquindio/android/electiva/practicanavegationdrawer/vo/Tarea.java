e101e68f7880bb083576cce24484bf69773e2f9d
Andres Osorio <andresangel115@hotmail.com>
2016-05-04 20:26:23 -0500

    Documentación de la clase
    Completa la documentación

--- src/main/java/co/edu/uniquindio/android/electiva/practicanavegationdrawer/vo/Tarea.java
+++ src/main/java/co/edu/uniquindio/android/electiva/practicanavegationdrawer/vo/Tarea.java
@@ -1,25 +1,41 @@
 package co.edu.uniquindio.android.electiva.practicanavegationdrawer.vo;
 
 /**
+ * Con esta clase se crearan las instancias de las tareas.
  * Created by Andrés Osorio on 04/05/2016.
  */
 public class Tarea {
     private String nombre;
     private String telefono;
 
+    /**
+     * Constructor de la clase por defecto sin parametros.
+     */
     public Tarea(String nombre, String telefono){
-
         this.nombre = nombre;
         this.telefono = telefono;
-
     }
 
+    /**
+     * Set del atributo nombre
+     */
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
+    
+    /**
+     * Set del atributo telefono
+     */
     public void setTelefono(String telefono){
         this.telefono = telefono;}
 
+    /**
+     * Get del atributo nombre
+     */
     public String getNombre(){return nombre;}
+    
+    /**
+     * Get del atributo telefono
+     */
     public String getTelefono(){return telefono;}
 }
